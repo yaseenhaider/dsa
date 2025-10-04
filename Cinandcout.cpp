49912555e068075b2aa072dc987ad53b6f8c8930
Yaseen Haider <yaseenhaiderabbas@gmail.com>
2025-10-04 08:38:45 -0700

    cover basic concept in C++

---
+++ Cinandcout.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    string s;
+
+    cout<<"type a number:";
+    //cout<<"your letter";
+
+    cin>>x;
+    //cin>>s;
+
+    cout<<"your number is:"<<x;
+    //cout<<" your letter is:"<<s;
+
+    return 0;
+}
